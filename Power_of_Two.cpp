854c5406eca10b8e9d41e43215f62e8624c56f7d
junaid-shaikh2 <junaid.shaikh.kn@gmail.com>
2025-03-23 22:20:18 +0500

    Added a file

--- Power_of_Two.cpp
+++ Power_of_Two.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello"<<endl;
-}
-
+class Solution {
+    public:
+        bool isPowerOfTwo(int n) {
+            int ans = 1;
+            // if(ans == n){
+            //     return true;
+            // }
+            for (int i=0; i<=30 ; i++){
+                if(ans == n){
+                    return true;
+                }
+                if(ans < INT_MAX/2)
+                ans = ans * 2;
+            }
+            return false;
+        }
+    };
